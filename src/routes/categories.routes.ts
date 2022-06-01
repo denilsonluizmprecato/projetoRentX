086f816344926ee0ae625b59a28bdce2236bf590
Denilson Luiz <denilson.martins@precato.com.br>
2022-06-01 13:57:55 -0300

    continuacao

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,14 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
-import { Category } from "../module/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepositories";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository;
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category: Category = {
-        name,
-        description,
-        id: uuidv4(),
-        created_at: new Date()
-    };
-
-    categories.push(category);
+    categoriesRepository.create({ name, description});
 
     return response.status(201).send();
 });
