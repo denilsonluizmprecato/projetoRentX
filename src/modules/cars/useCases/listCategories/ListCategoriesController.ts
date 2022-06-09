1a9e90b7c8f4a7e8b3d66c6ef2be20b531d7af34
Denilson Luiz <denilson.martins@precato.com.br>
2022-06-09 13:57:54 -0300

    iniciar cap 3

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express"
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-    handle(request: Request, response: Response): Response {
+    constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
 
+    handle(request: Request, response: Response): Response {
+        const all = this.listCategoriesUseCase.execute();
+
+        return response.json(all);
     }
 
 }
