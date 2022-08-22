0bdb3cb38f4ec99665bcfa969a193e6cf2c622e8
Denilson Luiz <denilson.martins@precato.com.br>
2022-08-22 14:01:24 -0300

    Caso de uso do cadastro de especificação para carro

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,32 @@
+import { CarsRepositoryInMemory } from "../../../../../src/modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "../../../../../src/shared/errors/AppError";
+import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase"
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car Specification", () => {
+
+    beforeEach(() => {
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+        createCarSpecificationUseCase = new CreateCarSpecificationUseCase(carsRepositoryInMemory);
+
+    });
+
+    it("should not be able to add a new specification to a not-existent car", async () => {
+        expect( async () => {
+            const car_id = "1234";
+            const specifications_id = ["54321"];
+            
+            await createCarSpecificationUseCase.execute({ car_id, specifications_id });
+        })
+    }).rejects.toBeInstanceOf(AppError);
+
+    it("should be able to add a new specification to the car", async () => {
+        const car_id = "1234";
+        const specifications_id = ["54321"];
+        
+        await createCarSpecificationUseCase.execute({ car_id, specifications_id });
+    });
+
+})
